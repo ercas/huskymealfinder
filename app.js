a20a8a9577d44a2ea1a0d2eb6cb6132163739ac1
ercas <edgarrimandocastro@gmail.com>
2017-01-05 20:53:36 -0500

    Added displayMenuMultiple function and supporting functions; implemented cache

--- app.js
+++ app.js
@@ -1,58 +1,77 @@
 var BASE_URL = "https://new.dineoncampus.com/v1/location/menu.json?platform=0";
-var MONTH_NAMES = [ "january", "february", "march", "april", "may", "june",
-    "july", "august", "september", "october", "november", "december" ];
+var MONTH_NAMES = [ "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December" ];
 var locations = {
-    "stwest": {
+    "Stwest": {
         "location_id": "586d05e4ee596f6e6c04b528",
         "site_id": "5751fd2b90975b60e048929a"
     },
-    "steast": {
+    "Steast": {
         "location_id": "586d05e4ee596f6e6c04b527",
         "site_id": "5751fd2b90975b60e048929a"
     },
-    "iv": {
+    "IV": {
         "location_id": "586d17503191a27120e60dec",
         "site_id": "5751fd2b90975b60e048929a"
     }
 };
 var requestsLoading = 0;
 
-var menus = {};
+var menuCache = {};
 
 /**
  * Builds a URL from an index of the locations table and a date string.
- * @param {string} targetLocation A string index of the locations table,
+ * @param {String} targetLocation A string index of the locations table,
  *     describing which location a menu should be retrieved for.
- * @param {string} dateString A yyyy-mm-dd formatted string corresponding to the
+ * @param {String} dateString A yyyy-mm-dd formatted string corresponding to the
  *     date that a menu should be retrieved for.
- * @return {string} A URL of a dineoncampus.com JSON containing the menu for the
+ * @return {String} A URL of a dineoncampus.com JSON containing the menu for the
  *     specified location and date
  */
 function buildUrl(targetLocation, dateString) {
     var url = BASE_URL + "&date=" + dateString;
 
-    for (var _location in locations)
-        if (_location == targetLocation)
-            for (key in locations[_location])
-                url += "&" + key + "=" + locations[_location][key];
+    for (var targetLocation in locations)
+        if (targetLocation == targetLocation)
+            for (key in locations[targetLocation])
+                url += "&" + key + "=" + locations[targetLocation][key];
 
     return url
 };
 
+/**
+ * Waits for all requests to finish before proceeding.
+ * @param {Number} timeout How long to wait between checks, in milliseconds.
+ * @param {Function} callback The function to call when all requests have
+ *     finished.
+ * @param {Function} waitingCallback A function that will be called every
+ *     recursion if not all requests have finished. The number of requests left
+ *     is passed to this function as an argument.
+ */
+function waitForRequests(callback, timeout, waitingCallback) {
+    if (!timeout)
+        timeout = 1000;
+    window.setTimeout(function() {
+        if (requestsLoading > 0) {
+            console.log("waiting for " + requestsLoading + " requests to "
+                        + "finish...");
+            if (waitingCallback)
+                waitingCallback(requestsLoading);
+            waitForRequests(callback, timeout, waitingCallback);
+        } else
+            callback();
+    }, timeout)
+}
 
 /**
  * Operates on an instance of MyClass and returns an array of items.
  * @param {Object} menuObject An object of a dineoncampus.com menu.json file.
  * @param {String} headerText Text that will be displayed as the menu's header.
  * @param {String} seachQuery Optional string that will filter menu items.
- * @return {Array} an array containing:
- *     {HTMLDivElement} A div containing divs of the dining periods containing
- *         divs of the food locations containing a table that contains a list of
- *         items served at that place at that time, each item being on a new
- *         row.
- *     {Number} A number describing how many results were found. If searchQuery
- *         was blank, this returns a number of all food items that were
- *         available on that day.
+ * @return {HTMLDivElement} A div containing divs of the dining periods
+ *     containing divs of the food locations containing a table that contains a
+ *     list of items served at that place at that time, each item being on a new
+ *     row.
  */
 function buildMenu(menuObject, headerText, searchQuery) {
     var container = document.createElement("div");
@@ -62,18 +81,18 @@ function buildMenu(menuObject, headerText, searchQuery) {
     header.innerHTML = headerText
     container.appendChild(header);
 
-    if (searchQuery) {
-        var queryInfo = document.createElement("p");
-        queryInfo.innerHTML = "showing results for \"" + searchQuery + "\"";
-        container.appendChild(queryInfo);
-    }
-
     if (menuObject) {
+        if (searchQuery) {
+            var queryInfo = document.createElement("p");
+            queryInfo.innerHTML = "Showing results for \"" + searchQuery + "\"";
+            container.appendChild(queryInfo);
+        }
         for (var a = 0; a < menuObject.periods.length; a++) {
             var period = menuObject.periods[a];
             var periodResults = 0;
 
             var periodContainer = document.createElement("div");
+            periodContainer.setAttribute("class", "nested-div");
 
             var periodHeader = document.createElement("h2");
             periodHeader.innerHTML = period.name;
@@ -84,6 +103,7 @@ function buildMenu(menuObject, headerText, searchQuery) {
                 var categoryResults = 0;
 
                 var categoryContainer = document.createElement("div");
+                categoryContainer.setAttribute("class", "nested-div");
 
                 var categoryHeader = document.createElement("h3");
                 categoryHeader.innerHTML = category.name;
@@ -112,36 +132,46 @@ function buildMenu(menuObject, headerText, searchQuery) {
                     }
                 }
 
-                if ((!searchQuery) || (searchQuery && (categoryResults > 0)))
+                if (categoryResults > 0)
                     periodContainer.appendChild(categoryContainer);
+                else if (!searchQuery) {
+                    // For completeness, empty categories will show that they
+                    // are empty if no query was performed.
+                    var itemRow = document.createElement("tr");
+                    itemRow.innerHTML = "Nothing :)";
+                    itemContainer.appendChild(itemRow);
+                    periodContainer.appendChild(categoryContainer);
+                }
             }
-            if ((!searchQuery) || (searchQuery && (periodResults > 0)))
+            if (periodResults > 0)
                 container.appendChild(periodContainer);
         }
-        if (searchQuery && dateResults == 0) {
+        if (dateResults == 0) {
             var notification = document.createElement("h2");
-            notification.innerHTML = "no results for \"" + searchQuery + "\" on "
-                                     + "the given date";
-            container.appendChild(notification);
+            if (searchQuery)
+                notification.innerHTML = "No results for \"" + searchQuery
+                                         + "\":(";
+            else
+                notification.innerHTML = "Nothing found :(";
+            periodContainer.innerHTML = "";
+            periodContainer.appendChild(notification);
+            container.appendChild(periodContainer);
         }
-    } else {
-        var notification = document.createElement("h2");
-        notification.innerHTML = "no data available for the given date";
-        container.appendChild(notification);
     }
 
-    return [container, dateResults];
+    return container;
 }
 
 /**
  * Operates on an instance of MyClass and returns something.
- * @param {string} targetLocation A string index of the locations table,
+ * @param {String} targetLocation A string index of the locations table,
  *     describing which location a menu should be loaded for.
  * @param {Date} dateObject The date that the menu should be retrieved for.
  * @param {Function} callback A function that is called when the menu JSON has
  *     been retrieved and loaded. The first argument passed is either the menu
  *     object or false; the second argument passed is a string containing a
- *     descriptive header generated by loadMenu.
+ *     descriptive header generated by loadMenu; the third argument passed is
+ *     the Unix time of the date.
  */
 function loadMenu(targetLocation, dateObject, callback) {
     var year = dateObject.getFullYear();
@@ -160,91 +190,97 @@ function loadMenu(targetLocation, dateObject, callback) {
     console.log("retrieving " + url);
     requestsLoading++
     $.getJSON(buildUrl(targetLocation, dateString), function(data){
-        if (!menus[targetLocation])
-            menus[targetLocation] = {};
-        if (data.menu) {
-            menus[targetLocation][dateString] = data.menu;
-            console.log("loaded " + targetLocation + " menu for " + dateString);
+        var menuAvailable = false
+
+        if (!menuCache[targetLocation])
+            menuCache[targetLocation] = {};
+
+        if (typeof(menuCache[targetLocation][dateString]) == "undefined") {
+            if (data.menu) {
+                menuCache[targetLocation][dateString] = data.menu;
+                console.log("cached " + targetLocation + " menu for "
+                            + dateString);
+                menuAvailable = true;
+            } else {
+                menuCache[targetLocation][dateString] = false;
+                console.log("could not retrieve " + targetLocation + " menu for"
+                            + dateString);
+            }
+        } else if (menuCache[targetLocation][dateString]) {
+            console.log("loaded " + targetLocation + " menu for " + dateString
+                        + " from cache");
+            menuAvailable = true
+        } else
+            console.log("cache indicated that " + targetLocation + " menu for "
+                        + dateString + " could not be retrieved previously");
+
+        if (menuAvailable)
             if (callback)
-                callback(data.menu, "menu for " + targetLocation + " on "
-                                    + dateStringReadable);
-        } else {
-            menus[targetLocation][dateString] = "nothing";
-            console.log("no menu available");
+                callback(data.menu, "Menu for " + targetLocation + " on "
+                                    + dateStringReadable, dateObject.getTime());
+        else
             if (callback)
-                callback(false, "no menu available for " + targetLocation
-                                + " on " + dateStringReadable);
-        }
+                callback(false, "No menu available for " + targetLocation
+                                + " on " + dateStringReadable,
+                         dateObject.getTime());
         requestsLoading--
     });
 }
 
 /**
- * Waits for all requests to finish before proceeding.
- * @param {Number} timeout How long to wait between checks, in milliseconds.
- * @param {Function} callback The function to call when all requests have
- *     finished.
- * @param {Function} waitingCallback A function that will be called every
- *     recursion if not all requests have finished.
- */
-function waitForRequests(callback, timeout, waitingCallback) {
-    if (!timeout)
-        timeout = 1000;
-    window.setTimeout(function() {
-        if (requestsLoading > 0) {
-            console.log("waiting for " + requestsLoading + " requests to "
-                        + "finish...");
-            if (waitingCallback)
-                waitingCallback(requestsLoading);
-            waitForRequests(callback, timeout, waitingCallback);
-        } else
-            callback();
-    }, timeout)
-}
-
-/**
- * A wrapper for buildMenu that appends the returned div to the document.
- * @param {Object} menuObject An object of a dineoncampus.com menu.json file.
- * @param {String} headerText Text that will be displayed as the menu's header.
- * @param {String} seachQuery Optional string that will filter menu items.
+ * A wrapper for buildMenu. For documentation, see buildMenu.
  */
 function displayMenu(menuObject, headerText, searchQuery) {
     main = document.getElementById("main");
     main.innerHTML = "";
-    main.appendChild(buildMenu(menuObject, headerText, searchQuery)[0]);
+    main.appendChild(buildMenu(menuObject, headerText, searchQuery));
 }
 
-var divs = [];
-function loadMenuMultiple(_location, dates, searchQuery) {
+/**
+ * Appends multiple menus to the main element
+ * @param {String} targetLocation A string index of the locations table,
+ *     describing which location a menu should be loaded for.
+ * @param {Array} dates An array of Date objects to be displayed
+ * @param {String} searchQuery An optional string used to filter results.
+ */
+function displayMenuMultiple(targetLocation, dates, searchQuery) {
     main = document.getElementById("main");
 
+    // A multidimensional array where:
+    //     i[0] is the Unix time of a menu's date
+    //     i[1] is the generated div of that menu
+    // This is useful for sorting the dates later.
+    var divs = [];
+
     for (var i = 0; i < dates.length; i++) {
-        loadMenu(_location, dates[i], function(menuObject, headerText) {
-            results = buildMenu(menuObject, headerText, searchQuery);
-            if (results[1] > 0)
-                divs.push(results[0]);
+        var dateObject = dates[i]
+        loadMenu(targetLocation, dateObject, function(menuObject, headerText, time) {
+            divs.push([time, buildMenu(menuObject, headerText, searchQuery)]);
         });
     }
 
     waitForRequests(function() {
         main.innerHTML = "";
+        // Display dates in chronological order
+        divs.sort(function(a, b) {
+            return a[0] - b[0];
+        });
         for (var i = 0; i < divs.length; i++){
-            main.appendChild(divs[i]);
+            main.appendChild(divs[i][1]);
         }
     }, 1000, function(requests) {
-        console.log("loop")
         var requestsWord = "requests";
         if (requests == 1)
             requestsWord = "request";
-        main.innerHTML = "waiting for " + requests + " " + requestsWord + " to "
+        main.innerHTML = "Waiting for " + requests + " " + requestsWord + " to "
                          + "finish...";
     })
 }
 
 document.getElementById("main").innerHTML = "loading...";
-//loadMenu("stwest", new Date(), displayMenu);
+//loadMenu("Stwest", new Date(), displayMenu);
 /*
-loadMenu("stwest", new Date(2017, 1, 9), function(menuObject, headerText) {
+loadMenu("Stwest", new Date(2017, 1, 9), function(menuObject, headerText) {
     displayMenu(menuObject, headerText, "pizza");
 });
 */
@@ -252,7 +288,8 @@ loadMenu("stwest", new Date(2017, 1, 9), function(menuObject, headerText) {
 dates =  [
     new Date(2017, 1, 9),
     new Date(2017, 1, 10),
-    new Date(2017, 1, 11)
+    new Date(2017, 1, 11),
+    new Date(2018, 1, 11)
 ]
 
-loadMenuMultiple("stwest", dates, "pizza");
+displayMenuMultiple("Stwest", dates);
